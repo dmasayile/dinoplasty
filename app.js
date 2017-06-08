e604b06d6191ffaca6ec6ae731cef197cfed1287
Davillious Masayile <user@users-mbp.coldspring.marian.edu>
2017-06-08 15:29:19 -0400

    Make lots of additions in entire project

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 const app = {
-    init(selectors) {
-        this.dinos = []
-        this.max = 0
-        this.list = document.querySelector(selectors.listSelector)
-        document
-            .querySelector(selectors.formSelector)
-            .addEventListener('submit', this.addDino.bind(this))
+  init(selectors) {
+    this.dinos = []
+    this.max = 0
+    this.list = document
+      .querySelector(selectors.listSelector)
+    this.template = document
+      .querySelector(selectors.templateSelector)
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', this.addDino.bind(this))
+  },
 
-
-    },
-addDino(ev) {
+  addDino(ev) {
     ev.preventDefault()
 
     const dino = {
-        id: this.max + 1,
-        name: ev.target.dinoName.value,
+      id: this.max + 1,
+      name: ev.target.dinoName.value,
     }
-    const listItem = this.renderListItem(dino)
-    this.list.appendChild(listItem)
-    ++this.max
-},
 
-renderListItem(dino) {
+    const listItem = this.renderListItem(dino)
+    this.list.insertBefore(listItem, this.list.firstChild)
+
+    this.dinos.unshift(dino)
+
+    ++ this.max
+    ev.target.reset()
+  },
+
+  renderListItem(dino) {
     const item = document.createElement('li')
     item.textContent = dino.name
+    item.dataset.id = dino.id
+
     return item
-}
+  }
 }
 
 app.init({
-    formSelector: '#dino-form',
-    listSelector: '#dino-list',
+  formSelector: '#dino-form',
+  listSelector: '#dino-list',
+  templateSelector: '.dino.template',
 })
